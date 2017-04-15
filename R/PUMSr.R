eddc9a1431f3cec6b1a02a1f9f8467a50228abac
elyasb <ebakhtiari@gmail.com>
2017-04-15 14:16:42 -0400

    Updated Description

--- R/PUMSr.R
+++ R/PUMSr.R
@@ -1,21 +1,19 @@
 # Function for reading IPUMS data using the DII codebook (xml)
 # Import the fixed-width data and use the IPUMS DII codebook file to import the positions
 
-require(XML)
-
 PUMSr <- function(dat, codebook, large=FALSE) {
   # Parse XML codebook
-  read <- xmlInternalTreeParse(codebook, useInternalNodes = TRUE)
+  read <- XML::xmlInternalTreeParse(codebook, useInternalNodes = TRUE)
   
   # Get the 'variable' nodeset
-  ns <- getNodeSet(read, "//*[name()='var']")
+  ns <- XML::getNodeSet(read, "//*[name()='var']")
   
   # Read the values of variable names
-  names <- xpathSApply(read,"//*[name()='var']",xmlAttrs) 
+  names <- XML::xpathSApply(read,"//*[name()='var']",xmlAttrs) 
   names <- names[1,]
   
   # Get start and end positions
-  pos <- xpathSApply(read, "//*[name()='var']/*[name()='location']", xmlAttrs)
+  pos <- XML::xpathSApply(read, "//*[name()='var']/*[name()='location']", xmlAttrs)
   startpos <- as.numeric(pos[1,])
   endpos <- as.numeric(pos[3,])
   
