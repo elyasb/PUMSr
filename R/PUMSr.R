ae3395d82e0687e812544d658bebb02975c4ccb6
elyasb <ebakhtiari@gmail.com>
2017-07-20 18:15:21 -0400

    Added category label option
    Now has an option for adding category labels to factor variables

--- R/PUMSr.R
+++ R/PUMSr.R
@@ -1,7 +1,7 @@
 # Function for reading IPUMS data using the DII codebook (xml)
 # Import the fixed-width data and use the IPUMS DII codebook file to import the positions
 
-PUMSr <- function(dat, codebook, large=FALSE) {
+pumsr <- function(dat, codebook, large=FALSE, labels=FALSE) {
   # Parse XML codebook
   read <- XML::xmlInternalTreeParse(codebook, useInternalNodes = TRUE)
   
@@ -9,30 +9,31 @@ PUMSr <- function(dat, codebook, large=FALSE) {
   ns <- XML::getNodeSet(read, "//*[name()='var']")
   
   # Read the values of variable names
-  names <- XML::xpathSApply(read,"//*[name()='var']",xmlAttrs) 
+  names <- XML::xpathSApply(read,"//*[name()='var']", XML::xmlAttrs) 
   names <- names[1,]
   
   # Get start and end positions
-  pos <- XML::xpathSApply(read, "//*[name()='var']/*[name()='location']", xmlAttrs)
+  pos <- XML::xpathSApply(read, "//*[name()='var']/*[name()='location']", XML::xmlAttrs)
   startpos <- as.numeric(pos[1,])
   endpos <- as.numeric(pos[3,])
   
   # Scrape label information
   # Get category value for each variable
   valpaths <- lapply(names, function(x) paste0("//*[name()='var'][@ID='", x, "']/*[name()='catgry']/*[name()='catValu']"))
-  catval <- lapply(valpaths, function(x) XML::xpathSApply(read, x, xmlValue))
+  catval <- suppressWarnings(lapply(valpaths, function(x) XML::xpathSApply(read, x, XML::xmlValue)))
   
   # Get category label for each value
   lblpath <- lapply(names, function(x) paste0("//*[name()='var'][@ID='", x, "']/*[name()='catgry']/*[name()='labl']"))
-  catlbl <- lapply(lblpath, function(x) xpathSApply(read, x, xmlValue))
-  type <- pos <- xpathSApply(read, "//*[name()='var']/*[name()='varFormat']", xmlAttrs)
+  catlbl <- suppressWarnings(lapply(lblpath, function(x) XML::xpathSApply(read, x, XML::xmlValue)))
+  type <- pos <- XML::xpathSApply(read, "//*[name()='var']/*[name()='varFormat']", XML::xmlAttrs)
   type <- type[2,]
+  coltype <- substr(gsub("numeric", "integer", type), start=1, stop=1)
   
   # For relatively small files, read in directly
   if(large==FALSE){
   #pums <- iotools::input.file(dat, formatter = dstrfw, col_types=type,
   #                   widths = (endpos - startpos) + 1)
-  pums <- readr::read_fwf(dat, readr::fwf_widths((endpos - startpos)+1))
+  pums <- as.data.frame(readr::read_fwf(dat, readr::fwf_widths((endpos - startpos)+1), col_type=paste(rep("c", 40), collapse="")))
   
   } else {
   # For larger files, use LaF package to load
@@ -42,7 +43,18 @@ PUMSr <- function(dat, codebook, large=FALSE) {
                              column_types=type)
     pums <- laf_to_ffdf(pums.laf)
   }
+  
+  if(labels==TRUE){
+  for(i in 1:length(pums)){ # Loops through and adds labels if available. Excludes some variables with more values than labels (such as year variables)
+    if(is.null(catlbl[[i]])==FALSE & (length(unique(catval[[i]])) >= length(unique(pums[,i])))==TRUE & (length(which(unique(pums[,i]) %in% catval[[i]]))>0)){
+      cat(paste("Adding category labels to", names[i], "\n"))
+      pums[,i] <- factor(pums[,i], levels=catval[[i]], labels=catlbl[[i]])
+    } else{class(pums[,i]) <- type[i]}
+  }
+  }
+  
   colnames(pums) <- tolower(names)
   return(pums)  
+  
 }
 
