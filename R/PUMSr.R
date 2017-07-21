48c0da9628b004cb100f6723617d28f3e3f477a8
elyasb <ebakhtiari@gmail.com>
2017-07-21 16:44:17 -0400

    Added cat labels for large files

--- R/PUMSr.R
+++ R/PUMSr.R
@@ -1,5 +1,4 @@
-#' Import fixed-width files from ipums.org directly into R using the corresponding XML IPUMS DII codebook.
-#' 
+#' Import fixed-width files from ipums.org directly into R
 #' @param dat The fixed-width data file.
 #' @param codebook The XML version of the codebook for the corresponding ipums extract.
 #' @param large Files designated as "large" are imported using the LaF and ffbase packages to avoid memory problems. Defaults to FALSE. Factor labels are currently not supported with large files.
@@ -33,29 +32,41 @@ pumsr <- function(dat, codebook, large=FALSE, labels=FALSE) {
   catlbl <- suppressWarnings(lapply(lblpath, function(x) XML::xpathSApply(read, x, XML::xmlValue)))
   type <- pos <- XML::xpathSApply(read, "//*[name()='var']/*[name()='varFormat']", XML::xmlAttrs)
   type <- type[2,]
-  coltype <- substr(gsub("numeric", "integer", type), start=1, stop=1)
-  
+
   # For relatively small files, read in directly
   if(large==FALSE){
   #pums <- iotools::input.file(dat, formatter = dstrfw, col_types=type,
   #                   widths = (endpos - startpos) + 1)
   pums <- as.data.frame(readr::read_fwf(dat, readr::fwf_widths((endpos - startpos)+1), col_type=paste(rep("c", 40), collapse="")))
   
+    # Convert to factors if labels==TRUE
+    if(labels==TRUE){
+      for(i in 1:length(pums)){ # Loops through and adds labels if available. Excludes some variables with more values than labels (such as year variables)
+        if(is.null(catlbl[[i]])==FALSE & (length(unique(catval[[i]])) >= length(unique(pums[,i])))==TRUE & (length(which(unique(pums[,i]) %in% catval[[i]]))>0)){
+          cat(paste("Adding category labels to", names[i], "\n"))
+          pums[,i] <- factor(pums[,i], levels=catval[[i]], labels=catlbl[[i]])
+        } else{class(pums[,i]) <- type[i]}
+      }
+    }
+  
   } else {
   # For larger files, use LaF package to load
     pums.laf <- LaF::laf_open_fwf(dat, column_widths=(endpos - startpos) + 1, 
-                             column_types=type)
-    pums <- ffbase::laf_to_ffdf(pums.laf)
+                             column_types=rep("string", length(type)))
+    pums <- ffbase::laf_to_ffdf(pums.laf, stringsAsFactors=FALSE)
+    
+    # Convert to factors if labels==TRUE
+    require(ff)
+      if(labels==TRUE){
+        for(i in 1:length(pums)){ # Loops through and adds labels if available. Excludes some variables with more values than labels (such as year variables)
+          if(is.null(catlbl[[i]])==FALSE & (length(unique(catval[[i]])) >= length(unique(pums[,i])))==TRUE & (length(which(unique(pums[,i]) %in% catval[[i]]))>0)){
+            cat(paste("Adding category labels to", names[i], "\n"))
+            virtual(pums[[i]])$Levels <- catlbl[[i]]
+            } else{assign(paste0("virtual(pums$V", i, ")$ramclass"), type[i])}
+        }
+      }
   }
   
-  if(labels==TRUE){
-  for(i in 1:length(pums)){ # Loops through and adds labels if available. Excludes some variables with more values than labels (such as year variables)
-    if(is.null(catlbl[[i]])==FALSE & (length(unique(catval[[i]])) >= length(unique(pums[,i])))==TRUE & (length(which(unique(pums[,i]) %in% catval[[i]]))>0)){
-      cat(paste("Adding category labels to", names[i], "\n"))
-      pums[,i] <- factor(pums[,i], levels=catval[[i]], labels=catlbl[[i]])
-    } else{class(pums[,i]) <- type[i]}
-  }
-  }
   
   colnames(pums) <- tolower(names)
   return(pums)  
